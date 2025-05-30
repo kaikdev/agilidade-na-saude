764a4bd52b38897019e957f25ae1dc2e5292f2b5
kaikdev <kaikdev.contato@gmail.com>
2025-05-29 21:16:03 -0300

    Create Dashboard: User

--- src/pages/user/DashboardUser.jsx
+++ src/pages/user/DashboardUser.jsx
@@ -1,3 +1,6 @@
+import ServiceRegistrationModal from './modals/ServiceRegistrationModal';
+import './DashboardUser.css';
+
 function DashboardUser() {
     return (
         <main className="main-dashboard">
@@ -5,6 +8,69 @@ function DashboardUser() {
                 <h3>Olá, Usuário!</h3>
                 <p>Seja bem-vindo ao seu Dashboard.</p>
             </div>
+
+            <section className="create-service">
+                <h3 className="section-title">
+                    <i className="fa-solid fa-briefcase-medical"></i>
+                    Atendimentos Disponíveis
+                </h3>
+
+                <div className="service-empty">
+                    <p>
+                        <i className="fa-solid fa-circle-exclamation"></i>
+                        Nenhum atendimento disponível.
+                    </p>
+                </div>
+
+                <div className="services-list">
+                    <div className="item-service">
+                        <div className="body-service">
+                            <h5>Titulo do Atendimento</h5>
+                            <div className="desc-service">
+                                <p>
+                                    <span>
+                                        <i className="fa-solid fa-calendar-days"></i>
+                                        Data:
+                                    </span>
+                                    29/05/2025
+                                </p>
+                            </div>
+                            <div className="desc-service">
+                                <p>
+                                    <span>
+                                        <i className="fa-solid fa-location-dot"></i>
+                                        Local:
+                                    </span>
+                                    Praça do Forró
+                                </p>
+                            </div>
+                            <div className="desc-service">
+                                <p>
+                                    <span>
+                                        <i className="fa-solid fa-list-ol"></i>
+                                        Senhas Disponíveis:
+                                    </span>
+                                    10
+                                </p>
+                            </div>
+
+                            <div className="buttons-actions">
+                                <button className="create-service" type="button" data-bs-toggle="modal" data-bs-target="#modalServiceRegistration">
+                                    <i className="fa-solid fa-user-plus"></i>
+                                    Participar
+                                </button>
+
+                                <button className="btn-edit" type="button">
+                                    <i className="fa-solid fa-circle-info"></i>
+                                    Mais Informações
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <ServiceRegistrationModal />
         </main>
     );
 }
---
+++ src/pages/user/modals/ServiceRegistrationModal.jsx
@@ -0,0 +1,80 @@
+import './ServiceRegistrationModal.css';
+
+function ServiceRegistrationModal() {
+    return (
+        /* Modal Inserir User em Atendimento */
+        <div className="modal fade" id="modalServiceRegistration" tabIndex="-1" aria-labelledby="modalServiceRegistrationLabel" aria-hidden="true">
+            <div className="modal-dialog modal-login modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-body">
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+
+                        <h6 className="modal-title" id="modalServiceRegistrationLabel">
+                            Gerar Senha de Atendimento
+                        </h6>
+
+                        <form>
+                            <div className="item-input mb-3">
+                                <label htmlFor="specialty">Selecionar Prioridades</label>
+                            </div>
+                                
+                                <div class="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" role="switch" id="priorities0" />
+
+                                    <label className="form-check-label" for="priorities0">
+                                        Nenhuma prioridade
+                                    </label>
+                                </div>
+
+                                <div class="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" role="switch" id="priorities1" />
+
+                                    <label className="form-check-label" for="priorities1">
+                                        PCD - Pessoa com Deficiência
+                                    </label>
+                                </div>
+
+                                <div class="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" role="switch" id="priorities2" />
+
+                                    <label className="form-check-label" for="priorities2">
+                                        Idoso
+                                    </label>
+                                </div>
+
+                                <div class="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" role="switch" id="priorities3" />
+
+                                    <label className="form-check-label" for="priorities3">
+                                        Grávida
+                                    </label>
+                                </div>
+
+                                <div class="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" role="switch" id="priorities4" />
+
+                                    <label className="form-check-label" for="priorities4">
+                                        Lactante
+                                    </label>
+                                </div>
+
+                                <div class="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" role="switch" id="priorities5" />
+
+                                    <label className="form-check-label" for="priorities5">
+                                        Mobilidade Reduzida (Temporária ou Permanente)
+                                    </label>
+                                </div>
+
+                            <button className="btn-default" type="submit">
+                                Gerar Senha
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ServiceRegistrationModal;
